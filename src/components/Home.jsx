7ae6486636aebff6ad609c7fe96ce44f16b0f9b8
Holm-Slice <taylorholm92@gmail.com>
2024-12-01 19:01:58 -0600

    converted some h2/h3 tags to h1 and added updated styling including custom font 2 and a new write up as an h2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,16 @@ function Home() {
         </div>
         <div className="bio">
           <br />
-          <WriteUp />
+          {/* <WriteUp /> */}
+          <h3 className="font">
+            Blanky crafts lush, reverb-soaked soundscapes and introspective
+            lyrics, blending Midwest melancholy with ethereal energy. Based in
+            Lawrence, Kansas, and Kansas City, the band is a vibrant force in
+            the local music scene, captivating shoegaze enthusiasts with their
+            dreamy, immersive performances. Their music bridges heartfelt
+            emotion and sonic exploration, carving a unique space in the
+            regional and broader shoegaze landscape.
+          </h3>
           <br />
         </div>
 
