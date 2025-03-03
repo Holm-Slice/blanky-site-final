6d802a57da27d0ea4423ae21911fce14707e63e1
Holm-Slice <taylorholm92@gmail.com>
2025-03-03 16:14:49 -0600

    word spacing and color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import WriteUp from "./WriteUp.jsx";
 function Home() {
   return (
     <>
-      <h1 className="font">Shoegaze from Lawrence Kansas</h1>
+      <h1 className="header-font">Shoegaze from Lawrence Kansas</h1>
       <div id="home-page">
         <div className="carousel-div">
           <Carousel data={slides} />
@@ -27,7 +27,7 @@ function Home() {
         </div>
 
         <div>
-          <h2 className="font">New album releasing soon!</h2>
+          <h2 className="new-album-font">New album releasing soon...</h2>
           <AlbumArt />
         </div>
       </div>
