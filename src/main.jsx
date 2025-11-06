183fe9643129134fa313092bf33149e4587d520e
Holm-Slice <taylorholm92@gmail.com>
2025-11-06 17:00:28 -0600

    Remove service worker and add cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,23 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+// --- Service worker + cache cleanup for old installs ---
+if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => {
+      registration.unregister();
+    });
+  });
+
+  if ("caches" in window) {
+    caches.keys().then((keys) => {
+      keys.forEach((key) => caches.delete(key));
+    });
+  }
+}
+
+// --- end cleanup ---
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
